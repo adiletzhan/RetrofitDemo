469887ac175adfe630a60a3909ee4a54ac840744
adiletzhan <adiletzhan.kambetov@gmail.com>
2021-11-24 23:35:15 +0600

    text_view problem solved

--- app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -2,23 +2,25 @@ package com.example.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
+import com.example.retrofitdemo.databinding.ActivityMainBinding
 import retrofit2.Response
-import retrofit2.create
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val retService: AlbumService = RetrofitInstance
-
             .getRetrofitInstance()
             .create(AlbumService::class.java)
-
 
         val responseLiveData: LiveData<Response<Albums>> = liveData {
             val response = retService.getAlbums()
@@ -33,11 +35,9 @@ class MainActivity : AppCompatActivity() {
                     val result = " " + "Album title: ${albumsItem.title}" + "\n" +
                             " " + "Album id: ${albumsItem.id}" + "\n" +
                             " " + "User id: ${albumsItem.userId}" + "\n\n\n"
-                    text_view.appen
-
+                    binding.textView.append(result)
                 }
             }
-
         })
     }
 
